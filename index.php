6a95f27502823282392a7c2d9ebd667e8d98c69a
taono <taotang@icloud.com>
2015-10-29 21:29:24 -0400

    index page setup

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="theme.css">
+</head>    
+<body>
+
+<h1>Welcome to my page</h1>
+
+<p>My first paragraph.</p>
+
+</body>
+</html>
